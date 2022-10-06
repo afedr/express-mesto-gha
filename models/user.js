5cda8e932de3aeb4a6e7ff1eb646292a34f551d9
Anastasia Fedorova <anfed722@gmail.com>
2022-10-07 01:03:28 +0400

    Turn on auth

--- models/user.js
+++ models/user.js
@@ -36,20 +36,22 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.statics.findUserByCredentials = (email, password) => this.findOne({ email }).select('+password')
-  .then((user) => {
-    if (!user) {
-      return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
-    }
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+      }
 
-    return bcrypt.compare(password, user.password)
-      .then((matched) => {
-        if (!matched) {
-          return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
-        }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+          }
 
-        return user;
-      });
-  });
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
