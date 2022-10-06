3f6078fe517713609e02653f1db620e7e7517c50
Anastasia Fedorova <anfed722@gmail.com>
2022-10-07 00:46:41 +0400

    fixed one more error handling

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const isEmail = require('validator/lib/isEmail');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const UnauthorizedError = require('../errors/ForbiddenError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -38,13 +39,13 @@ const userSchema = new mongoose.Schema({
 userSchema.statics.findUserByCredentials = (email, password) => this.findOne({ email }).select('+password')
   .then((user) => {
     if (!user) {
-      return Promise.reject(new Error('Неправильные почта или пароль'));
+      return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
     }
 
     return bcrypt.compare(password, user.password)
       .then((matched) => {
         if (!matched) {
-          return Promise.reject(new Error('Неправильные почта или пароль'));
+          return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
         }
 
         return user;
